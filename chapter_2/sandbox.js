b11b4424f9b72bc8a5aeef7139037b16530d2464
nalmario <noahralmario@gmail.com>
2025-05-08 10:15:57 -0500

    chapter 2 - completed strings section in sandbox.js

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -3,21 +3,59 @@
 /* console.log(1);
 console.log(2); */
 
-// let - variable assignment
-//     - use camelCase (don't start name with a number or use keywords)
-let myAge = 25;
-let year = 2019;
 
-console.log(myAge, year);
+// // let - variable assignment
+// //     - use camelCase (don't start name with a number or use keywords)
+// let myAge = 25;
+// let year = 2019;
 
-myAge = 30;
-console.log(myAge);
+// console.log(myAge, year);
 
-// const - variables that will not be changing
-const points = 100;
-// points = 50;
-console.log(points);
+// myAge = 30;
+// console.log(myAge);
 
-// var - the old way of variable assignment (before let + const)
-var score = 75;
-console.log(score);
+// // const - variables that will not be changing
+// const points = 100;
+// // points = 50;
+// console.log(points);
+
+// // var - the old way of variable assignment (before let + const)
+// var score = 75;
+// console.log(score);
+
+
+// // strings
+// console.log('hello, world');
+
+// let email = 'noahralmario@gmail.com';
+// console.log(email);
+
+// // string concatenation
+// let firstName = 'Brandon';
+// let lastName = 'Sanderson';
+
+// let fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+// // getting characters
+// console.log(fullName[2]); // square brackets will grab the character at the provided index
+
+// // string length
+// console.log(fullName.length);
+
+// // string methods
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase();
+// console.log(result, fullName);
+
+// let index = email.indexOf('@');
+// console.log(index);
+
+// // common string methods
+// result = email.lastIndexOf('o');
+// result = email.slice(0,5);
+// result = email.substr(4, 8)
+// result = email.replace('n', 'w'); // only replaces first instance of 'n' in string
+// console.log(result);
+
+
