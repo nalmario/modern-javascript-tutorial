0b85078707e3d5a08c714fa35354f994bd41c348
nalmario <noahralmario@gmail.com>
2025-05-08 14:41:01 -0500

    chapter 4 - completed arrow functions in sandbox.js

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -22,15 +22,49 @@
 
 
 // // returning values
-const calcArea = function(radius){
-  return 3.14 * radius**2
+// const calcArea = function(radius){
+//   return 3.14 * radius**2
+// };
+
+// const area = calcArea(5);
+// console.log(area);
+
+// const calcVol = function(area){
+//   // return calculation here
+// };
+
+// calcVol(area);
+
+
+// // arrow function syntax, just a newer and shorter way of writing out functions
+// const calcArea = radius => 3.14 * radius**2;
+
+// const area = calcArea(5);
+// console.log('area is:', area);
+
+// // practice
+// const greet = function(){
+//   return 'hello, world';
+// };
+
+// const greet = () => 'hello,world';
+// const result = greet();
+// console.log(result);
+
+// const bill = function(products, tax){
+//   let total = 0;
+//   for(let i = 0; i < products.length; i++){
+//     total += products[i] + products[i] * tax;
+//   }
+//   return total;
+// };
+
+const bill = (products, tax) => {
+  let total = 0;
+  for(let i = 0; i < products.length; i++){
+    total += products[i] + products[i] * tax;
+  }
+  return total;
 };
 
-const area = calcArea(5);
-console.log(area);
-
-const calcVol = function(area){
-  // return calculation here
-};
-
-calcVol(area);
+console.log(bill([10,15,30], 0.2));
