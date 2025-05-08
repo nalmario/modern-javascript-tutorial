f5c15331a706a8d8f343275286a11930700793bc
nalmario <noahralmario@gmail.com>
2025-05-08 08:41:44 -0500

    chapter 2 - completed variable section

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -3,3 +3,21 @@
 /* console.log(1);
 console.log(2); */
 
+// let - variable assignment
+//     - use camelCase (don't start name with a number or use keywords)
+let myAge = 25;
+let year = 2019;
+
+console.log(myAge, year);
+
+myAge = 30;
+console.log(myAge);
+
+// const - variables that will not be changing
+const points = 100;
+// points = 50;
+console.log(points);
+
+// var - the old way of variable assignment (before let + const)
+var score = 75;
+console.log(score);
