a43aa1e40764cac8e5fb75a666cb368acdfd6dae
nalmario <noahralmario@gmail.com>
2025-05-08 13:25:09 -0500

    chapter 3 - completed logical NOT section in sandbox.js

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -71,3 +71,14 @@
 // } else {
 //   console.log('password is not strong enough!')
 // }
+
+
+// logical NOT (!)
+// let user = false;
+
+// if(!user){
+//   console.log('you must be logged in to continue')
+// }
+
+// console.log(!true);
+// console.log(!false);
