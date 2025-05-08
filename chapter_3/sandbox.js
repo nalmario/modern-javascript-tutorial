307df2e2397c93b9cc4f461337dfb51b7a9ac94b
nalmario <noahralmario@gmail.com>
2025-05-08 13:11:58 -0500

    chapter 3 - completed if else statements section in sandbox.js

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -35,3 +35,27 @@
 //   i++
 // }
 // while(i < 5)
+
+
+// // if statements
+// const age = 25;
+
+// if(age > 20){
+//   console.log('you are over 20 years old');
+// }
+
+// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi']
+
+// if(ninjas.length > 4){
+//   console.log("that's a lot of ninjas");
+// }
+
+const password = 'p@ss'
+
+if(password.length >= 12){
+  console.log('password is mighty strong!')
+} else if(password.length >= 8) {
+  console.log('that password is long enough!')
+} else {
+  console.log('password is not long enough!')
+}
