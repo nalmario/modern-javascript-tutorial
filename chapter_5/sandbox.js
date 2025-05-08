04ea2b704538ddb637d548de7429c7c2a4a40590
nalmario <noahralmario@gmail.com>
2025-05-08 15:35:54 -0500

    chapter 5 - completed object literals in sandbox.js

---
+++ chapter_5/sandbox.js
@@ -0,0 +1,20 @@
+// // object literals
+let user = {
+  name: 'crystal',
+  age: 30,
+  email: 'crystal@thenetninja.co.uk',
+  location: 'berlin',
+  blogs: ['why mac and cheese rules', '10 things to make with marmite']
+};
+
+console.log(user);
+console.log(user.name);
+
+// user.age = 35;
+console.log(user.age);
+
+console.log(user['email']);
+user['name'] = 'chun-li';
+console.log(user['name']);
+
+console.log(typeof user);
