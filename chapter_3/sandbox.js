21cba2b0381ea3071fbafee267376e30e33d8d31
nalmario <noahralmario@gmail.com>
2025-05-08 13:22:01 -0500

    chapter 3 - completed logical operators section in sandbox.js

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -62,12 +62,12 @@
 
 
 // // logical operators - OR || AND &&
-const password = 'p@ss12';
+// const password = 'p@ss12';
 
-if(password.length >= 12 && password.includes('@')){
-  console.log('password is mighty strong!')
-} else if(password.length >= 8 || password.includes('@') && password.length >= 5) {
-  console.log('that password is strong enough!')
-} else {
-  console.log('password is not strong enough!')
-}
+// if(password.length >= 12 && password.includes('@')){
+//   console.log('password is mighty strong!')
+// } else if(password.length >= 8 || password.includes('@') && password.length >= 5) {
+//   console.log('that password is strong enough!')
+// } else {
+//   console.log('password is not strong enough!')
+// }
