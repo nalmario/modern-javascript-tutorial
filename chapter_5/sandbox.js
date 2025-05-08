1b9626a821c2dc7cea6abc3c3608fd0c657a503a
nalmario <noahralmario@gmail.com>
2025-05-08 15:46:40 -0500

    chapter 5 - completed object methods and 'this' keyword in sandbox.js

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -1,20 +1,47 @@
 // // object literals
+// let user = {
+//   name: 'crystal',
+//   age: 30,
+//   email: 'crystal@thenetninja.co.uk',
+//   location: 'berlin',
+//   blogs: ['why mac and cheese rules', '10 things to make with marmite']
+// };
+
+// console.log(user);
+// console.log(user.name);
+
+// // user.age = 35;
+// console.log(user.age);
+
+// console.log(user['email']);
+// user['name'] = 'chun-li';
+// console.log(user['name']);
+
+// console.log(typeof user);
+
+
+// // object methods and 'this'
 let user = {
   name: 'crystal',
   age: 30,
   email: 'crystal@thenetninja.co.uk',
   location: 'berlin',
-  blogs: ['why mac and cheese rules', '10 things to make with marmite']
+  blogs: ['why mac and cheese rules', '10 things to make with marmite'],
+  login(){
+    console.log('the user logged in');
+  },
+  logout(){
+    console.log('the user logged out');
+  },
+  logBlogs(){
+    // console.log(this.blogs)
+    console.log('this user has written the following blogs:')
+    this.blogs.forEach(blog => {
+      console.log(blog);
+    })
+  }
 };
 
-console.log(user);
-console.log(user.name);
-
-// user.age = 35;
-console.log(user.age);
-
-console.log(user['email']);
-user['name'] = 'chun-li';
-console.log(user['name']);
-
-console.log(typeof user);
+// user.login();
+// user.logout();
+user.logBlogs();
