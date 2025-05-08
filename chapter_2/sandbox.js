866f6097e2035798971c87d941d02d45883564d9
nalmario <noahralmario@gmail.com>
2025-05-08 10:27:20 -0500

    chapter 2 - completed number operations section in sandbox.js

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -59,3 +59,35 @@ console.log(2); */
 // console.log(result);
 
 
+// // numbers
+// let radius = 10;
+// const pi = 3.14;
+
+// console.log(radius, pi);
+
+// // math operators +, -, *, /, **, %
+// // REMEMBER BEDMAS
+// console.log(10 / 2);
+// // let result = radius % 3
+// let result = pi * radius**2
+
+// console.log(result)
+
+// let likes = 10
+
+//likes = likes + 1;
+//likes++;
+//likes--
+//likes += 10;
+//likes -= 5;
+//likes *= 2;
+//likes /= 2;
+
+//console.log(likes);
+
+// NaN - not a number
+// console.log(5 / 'hello');
+// console.log(5 * 'hello');
+
+// let result = 'the blog has ' + likes + ' likes';
+// console.log(result);
