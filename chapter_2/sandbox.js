247b6f9e77a8e80c7f7f86b8f02ab90e73273457
nalmario <noahralmario@gmail.com>
2025-05-08 11:18:03 -0500

    chapter 2 - completed booleans and comparisons section in sandbox.js

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -133,3 +133,31 @@ console.log(2); */
 // let result = ninjas.pop(); // removes last item in array, returns the removed item
 // console.log(ninjas);
 
+
+// booleans & comparisons
+// console.log(true, false);
+
+// methods can return booleans
+// let email = 'luigi@thenetninja.co.uk';
+// let names = ['mario', 'luigi', 'toad']
+
+// let result = email.includes('@'); // checks if a character is included in a string, will return true or false
+// let result = names.includes('luigi');
+// console.log(result);
+
+// comparison operators
+// let age = 25;
+// console.log(age == 25);
+// console.log(age == 30);
+// console.log(age != 30);
+// console.log(age > 20);
+// console.log(age < 20);
+// console.log(age >= 25);
+// console.log(age <= 25);
+
+// let name = 'shaun';
+// console.log(name == 'shaun');
+// console.log(name == 'Shaun');
+// console.log(name > 'crystal'); // true, s comes later than c in the alphabet
+// console.log(name > 'Shaun'); // true, lowercase letters are greater than any uppercase letter
+// console.log(name > 'Crystal'); // true
