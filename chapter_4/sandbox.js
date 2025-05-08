0b12d3f54aec12f90bc4b3ec17fc80c8d974980e
nalmario <noahralmario@gmail.com>
2025-05-08 15:18:19 -0500

    chapter 4 - completed forEach and html template in sandbox.js
    chapter 4 - modified index.html to demonstrate forEach

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -84,14 +84,29 @@
 
 
 // // callbacks & foreach
-let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+// let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
 
-const logPerson = (person, index) => {
-  console.log(`${index} - hello, ${person}`);
-};
+// const logPerson = (person, index) => {
+//   console.log(`${index} - hello, ${person}`);
+// };
 
-people.forEach(logPerson);
+// people.forEach(logPerson);
 
 // people.forEach((person, index) => {
 //   console.log(index, person);
 // });
+
+
+// // get a reference to the 'ul'
+const ul = document.querySelector('.people'); // uses css selector to get the ul element
+const people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+let html = ``
+
+people.forEach(person => {
+  // create html template
+  html += `<li style="color: purple">${person}</li>`
+});
+
+console.log(html);
+ul.innerHTML = html;
