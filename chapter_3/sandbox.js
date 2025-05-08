f71b9783cf66fd692587c37c1c9803ef7bf083ce
nalmario <noahralmario@gmail.com>
2025-05-08 13:04:45 -0500

    chapter 3 - completed while loops in sandbox.js

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,13 +1,37 @@
-// for loops
+// // for loops
 // for(let i = 0; i < 5; i++){
 //   console.log('in loop:', i)
 // }
 // console.log('loop complete');
 
-const names = ['shaun', 'mario', 'luigi'];
+// const names = ['shaun', 'mario', 'luigi'];
 
-for(let i = 0; i < names.length; i++){
-  // console.log(names[i]);
-  let html = `<div>${names[i]}</div>`;
-  console.log(html);
-}
+// for(let i = 0; i < names.length; i++){
+//   // console.log(names[i]);
+//   let html = `<div>${names[i]}</div>`;
+//   console.log(html);
+// }
+
+
+// // while loops
+// const names = ['shaun', 'mario', 'luigi'];
+// let i = 0;
+// while(i < 5){
+//   console.log('in loop: ', i);
+//   i++;
+// }
+
+// while(i < names.length){
+//   console.log(names[1]);
+//   i++;
+// }
+
+
+// // do while loops
+// let i = 5;
+
+// do{
+//   console.log('val of i is: ', i);
+//   i++
+// }
+// while(i < 5)
