fd93298b2d2ed7a3cd3c68184ebef31631030e9c
nalmario <noahralmario@gmail.com>
2025-05-08 10:36:22 -0500

    chapter 2 - completed template strings section in sandbox.js

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -91,3 +91,24 @@ console.log(2); */
 
 // let result = 'the blog has ' + likes + ' likes';
 // console.log(result);
+
+
+// // template strings
+// const title = 'Best reads of 2019';
+// const author = 'Mario';
+// const likes = 30;
+
+// // template string "concatenating"
+// let result = `The blog called ${title} by ${author} has ${likes} likes.`;
+// console.log(result);
+
+// // creating html templates
+// let html = `
+//   <h2>${title}</h2>
+//   <p>By ${author}</p>
+//   <span>This blog has ${likes} likes</span>
+// `;
+// console.log(html);
+
+
+//
