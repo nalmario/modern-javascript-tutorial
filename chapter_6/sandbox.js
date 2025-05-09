8542b3229538a58094a8406ac4c417d5ab80e504
nalmario <noahralmario@gmail.com>
2025-05-09 09:57:58 -0500

    chapter 6 - added an id for h1 tag in index.html, completed get elements in sandbox.js

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -10,5 +10,20 @@
 //   console.log(para.innerHTML); // innerHTML grabs the text inside of the tag
 // })
 
-const errors = document.querySelectorAll('.error');
-console.log(errors)
+// const errors = document.querySelectorAll('.error');
+// console.log(errors)
+
+
+// // get an element by ID
+// const title = document.getElementById('page-title'); // html ids must be unique
+// console.log(title)
+
+// // get elements by class name
+// const errors = document.getElementsByClassName('error');
+// console.log(errors)
+
+
+// get elements by tag name
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[1]);
