b7cf43bc7d7536c41a898c6422d6c06146006f8f
nalmario <noahralmario@gmail.com>
2025-05-09 09:44:43 -0500

    chapter 6 - modified index.html with added p tags and ones of error class
    completed querySelector() and querySelectorAll in sandbox.js

---
+++ chapter_6/sandbox.js
@@ -0,0 +1,14 @@
+// // querySelector()
+// const para = document.querySelector('div.error'); // grabs the tag that comes first
+// console.log(para.innerHTML);
+
+// // querySelectorAll()
+// const paras = document.querySelectorAll('p'); // grabs all tags inputted
+// console.log(paras[0]);
+
+// paras.forEach(para => {
+//   console.log(para.innerHTML); // innerHTML grabs the text inside of the tag
+// })
+
+const errors = document.querySelectorAll('.error');
+console.log(errors)
