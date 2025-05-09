38ed161ad7a0448c838a4a72098d3abe4e2d0bf8
nalmario <noahralmario@gmail.com>
2025-05-09 10:30:24 -0500

    chapter 6 - added content section in index.html, completed inner text and html manipulation in sandbox.js

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -7,7 +7,7 @@
 // console.log(paras[0]);
 
 // paras.forEach(para => {
-//   console.log(para.innerHTML); // innerHTML grabs the text inside of the tag
+//   console.log(para.innerHTML);
 // })
 
 // const errors = document.querySelectorAll('.error');
@@ -23,7 +23,29 @@
 // console.log(errors)
 
 
-// get elements by tag name
-const paras = document.getElementsByTagName('p');
-console.log(paras);
-console.log(paras[1]);
+// // get elements by tag name
+// const paras = document.getElementsByTagName('p');
+// console.log(paras);
+// console.log(paras[1]);
+
+
+// // change inner text
+// const para = document.querySelector('p');
+// console.log(para.innerText); // innerText grabs the text inside of the tag
+// para.innerText = 'ninjas are awesome';
+
+// const paras = document.querySelectorAll('p');
+// paras.forEach(para => {
+//   console.log(para.innerText);
+//   para.innerText += ' new text';
+// })
+
+// // change inner HTML (includes tags)
+// const content = document.querySelector('.content');
+// console.log(content.innerHTML);
+// content.innerHTML += '<h2>THIS IS A NEW H2</h2>'
+
+// const people = ['mario', 'luigi', 'yoshi'];
+// people.forEach(person => {
+//   content.innerHTML += `<p>${person}</p>`
+// })
